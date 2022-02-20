f91801b4241b0144646e94319806f1b1e7a76ebb
nishi18masa@gmail.com <masa18nishi@gmail.com>
2022-02-20 13:38:22 +0900

    second commit

--- js/main.js
+++ js/main.js
@@ -42,11 +42,46 @@
 // }
 
 // do...while文
-let number = 0;
+// let number = 0;
 
-do {
-  console.log(number);
-  number++;
-} while (number <= 10);
+// do {
+//   console.log(number);
+//   number++;
+// } while (number <= 10);
 
+// 練習問題
+// 1.
+let test = ["国語", "数学", "英語"];
+console.log(test);
 
+test.push("理科", "社会");
+console.log(test);
+
+console.log(test.length);
+
+// 2
+for (i = 1; i <= 50; i++){
+  if (i % 15 == 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 == 0) {
+    console.log("Fizz");
+  } else if (i % 5 == 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+}
+
+// 3
+let water = 1000;
+let d_water = water - 180;
+
+while (water >= 0) {
+  if (d_water >= 0) {
+    d_water = water - 180;
+  }
+  console.log(d_water);
+  break;
+}
+
+console.log("水を飲み干した");
