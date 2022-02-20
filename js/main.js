bcd7e28dd90d20b410d2265f483d98c2271ee338
nishi18masa@gmail.com <masa18nishi@gmail.com>
2022-02-20 09:21:51 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// 繰り返し処理
+// for文
+// for文を使わない場合
+// const list01 = ["山田", "佐藤", "山下", "櫻井", "相葉", "松本"];
+
+// console.log(list01[0]);
+// console.log(list01[1]);
+// console.log(list01[2]);
+// console.log(list01[3]);
+// console.log(list01[4]);
+// console.log(list01[5]);
+
+// for文を使った場合
+// const list02 = ["山田", "佐藤", "山下", "櫻井", "相葉", "松本"];
+
+// for (i = 0; i < list02.length; i++){
+//   console.log(list02[i]);
+// }
+
+// while文
+const number = 15;
+
+while (number <= 15) {
+  numbers = 15 - number;
+  console.log(numbers);
+}
